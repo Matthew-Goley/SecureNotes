6ad4e361c0a78a5f47bdfd727f5af744dbd5bb5b
Matthew-Goley <nutmeg278@pm.me>
2026-04-18 20:57:04 -0500

    Removed debug routes that were potential vulnribilities

--- backend/src/routes/debug.ts
+++ backend/src/routes/debug.ts
@@ -3,14 +3,6 @@ import db from "../db";
 import { authenticate } from "../middleware/authenticate";
 
 export default async function debugRoutes(app: FastifyInstance) {
-    // UserList GET (For debugging)
-    app.get("/userlist", async () => {
-        const userlist = db.prepare("SELECT * FROM users").all();
-        return {
-            users: userlist
-        };
-    });
-
     // Protected route
     app.get("/protected", { preHandler: authenticate }, async (request: any) => {
         return { message: `Hello ${request.user.username}` };
